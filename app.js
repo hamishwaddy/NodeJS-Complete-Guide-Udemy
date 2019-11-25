a5b36fe3b79f3949e3cdab8bb3b402b618d8cb92
Hamish Wadsworth <hamishwaddy@gmail.com>
2019-11-25 14:55:28 +1300

    image upload adjustment

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
     // https
     //   .createServer({ key: privateKey, cert: certificate }, app)
